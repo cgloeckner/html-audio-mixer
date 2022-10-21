c7f22f7cde58a1ee4c6e414b34b26359f462b1a2
cgloeckner <cgloeckner@freenet.de>
2022-10-21 11:43:22 +0200

    Refactoring

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 This is meant as a tool used when playing Roleplaying Tabletop / Pen & Paper games. It can be used to play two audio tracks at once and changing the volume balance between both.
 
 You can directly use the tool in your browser via https://cgloeckner.github.io/html-audio-mixer
+
+## Usage
+
+Upload audio files. Play them in channel A or B and adjust the volume.
+
+*Pending Feature:* Restoring previously loaded audio (note: may be too large for `localStorage`, needs some work)
+
--- mixer.js
+++ mixer.js
@@ -1,69 +1,141 @@
-var upload_prefix = null
+/**
+https://github.com/cgloeckner/html-audio-mixer
 
-function play(prefix) {
-    let src = $('#' + prefix + '_list :selected').val()
-    let player = $('#' + prefix + '_player')[0]
-    if (src != '') {
-        player.src = src
-        player.play()
-    } else {
-        player.pause()
-    }
-}
+Copyright (c) Christian Glöckner
+License: MIT (see LICENSE for details)
+*/
 
-function setVolume(prefix, volume) {
-    let player = $('#' + prefix + '_player')[0]
+var next_id = 0
 
-    player.volume = volume / 100.0
-}
+/// Start playback on given channel
+play = (id) => {
+    let label = $(`#label${id}`)
+    let channel = $(`#channel${id}`)
+    let option = $('#tracks :selected')
 
-function mix() {
-    let vol = parseInt($('#balance').val())
-    
-    setVolume('ambience', 100-vol)
-    setVolume('tone', vol)
-}
-
-function showTrack(prefix, tag, title, selected=false) {
-    let target = $('#' + prefix + '_list')[0]
-
-    var item = new Option()
-    item.value = tag
-    item.innerHTML = title
-    if (selected) {
-        item.setAttribute('selected', 'selected')
-    }
-    target.appendChild(item)
-}
-
-function add(prefix) {
-    let source = $('#upload')[0]
-    upload_prefix = prefix
-    source.click()
-}
-
-function startUpload() {   
-    let source = $('#upload')
-    
-    // iterate all selected files
-    $.each(source[0].files, function(index, obj) {
-        let url = (window.URL || window.webkitURL).createObjectURL(obj)
+    if (option.val() != null) {
+        label[0].innerHTML = option.text()
+        channel[0].src = option.val()
+        channel[0].play()
+        next_id = (id + 1) % 2
         
-        showTrack(upload_prefix, url, obj.name)
+    } else {
+        label[0].innerHTML = '&lt;PAUSE&gt;'
+        channel[0].src = null
+        channel[0].pause()
+        next_id = id
+    }
+     
+}
+
+/// Stop playback on given channel
+stop = (id) => {
+    let label = $(`#label${id}`)
+    let channel = $(`#channel${id}`)
+    
+    label[0].innerHTML = '&lt;PAUSE&gt;'
+    channel[0].src = null
+    
+    next_id = id
+}
+
+/// Set volume mix for both channels at once
+mix = () => {
+    let volume = parseInt($('#balance').val())
+    let channel0 = $('#channel0')
+    let channel1 = $('#channel1')
+
+    channel0[0].volume = (100 - volume) / 100.0
+    channel1[0].volume = volume / 100.0
+}
+
+/// Browse for files
+browse = () => {
+    let source = $('#files')
+    source[0].click()
+}
+
+async function load(file) {
+    let base64 = await new Promise((resolve) => {
+        let reader = new FileReader()
+        reader.onload = (e) => resolve(reader.result)
+        reader.readAsDataURL(file)
+    })
+
+    return base64
+}
+
+/// Upload selected files
+upload = () => {
+    let source = $('#files')
+    let files = source[0].files
+    
+    $.each(source[0].files, (index, obj) => {
+        const reader = new FileReader()
+        reader.onload = function() {
+            add(this.result, obj.name)
+        }
+        reader.readAsDataURL(obj)
     })
     
-    upload_prefix = null
     source.val("")
 }
 
-function clearAll(prefix) {
-    let q = 'Really clear all ' + prefix + 's?'
+/// Add given track to tracklist
+add = (uri, title) => {
+    let tracks = $('#tracks')
+
+    // check for duplicate
+    let skip = false
+    $.each(tracks[0], (index, option) => {
+        if (option.innerHTML == title) {
+            skip = true
+            return
+        }
+    })
+    if (skip) {
+        return
+    }
     
-    if (confirm(q)) {
-        let target = $('#' + prefix + '_list')
-        target.empty()
-        showTrack(prefix, '', '---', true)
-        
-        play(prefix)
+    // save to browser cache
+    // @NOTE: disabled (file size may exceed localStorage)
+    //localStorage.setItem(`title:${title}`, uri)
+
+    // add to list
+    let item = new Option()
+    item.value = uri
+    item.innerHTML = title
+    tracks[0].appendChild(item)
+}
+ 
+/// Remove selected track from tracklist
+remove = () => {
+    let option = $('#tracks :selected')
+
+    // remove from browser cache
+    // @NOTE: disabled
+    //let key = `title:${option[0].innerHTML}`
+    //localStorage.removeItem(key)
+
+    // remove from list
+    option.remove()
+}
+
+/// Initialize audio mixer
+init = () => {
+    let len = localStorage.length
+    for (var i = 0; i < len; ++i) {
+        let key = localStorage.key(i)
+        if (!key.startsWith('title:')) {
+            continue
+        }
+
+        // parse data
+        let title = key.split('title:')[1]
+        let uri = localStorage.getItem(key)
+
+        // @NOTE: disabled
+        //add(uri, title)
+        //console.log(`Loaded ${title}`)
     }
 }
